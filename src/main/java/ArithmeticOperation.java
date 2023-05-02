323076ece58c6d3642650c9847323761a13f39d7
vinit <vinitkkulkarni@gmail.com>
2023-05-02 17:38:52 +0530

    Added:multiplication method

--- src/main/java/ArithmeticOperation.java
+++ src/main/java/ArithmeticOperation.java
@@ -6,6 +6,10 @@ public class ArithmeticOperation {
     public int substract(int x, int y){
         return x-y;
     }
+
+    public int multiply(int x, int y){
+        return x*y;
+    }
     public static void main(String[] args) {
         System.out.println("Arithmetic operations program");
     }
